1a993e7ac4fce446ba5d9ddb7fcb0410c74cdcba
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2020-07-07 13:28:18 +0300

    assert module

--- index.test.js
+++ index.test.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const { forEach, map } = require('./index');
 
 const test = (desc, fn) => {
@@ -15,22 +16,12 @@ test('The forEach function', () => {
 		sum += value;
 	});
 
-	if (sum !== 7) {
-		throw new Error('Expected summing array to equal 6');
-	}
+	assert.strictEqual(sum, 6);
 });
 
 test('The map function', () => {
 	const result = map([ 1, 2, 3 ], (value) => {
 		return value * 2;
 	});
-	if (result[0] !== 2) {
-		throw new Error(`Expected to find 2, but found ${result[0]}`);
-	}
-	if (result[1] !== 4) {
-		throw new Error(`Expected to find 4, but found ${result[1]}`);
-	}
-	if (result[2] !== 6) {
-		throw new Error(`Expected to find 6, but found ${result[2]}`);
-	}
+	assert.deepStrictEqual(result, [ 2, 4, 6 ]);
 });
